3c39ca8c59cd3e4112e2586b9fce8db8ea0cdff7
Paulo Henrique Alves <ph.alves@live.com>
2013-05-29 20:11:33 -0300

    Removida biblioteca não usada de Logout

--- src/br/com/trab3/Servlet/Logout.java
+++ src/br/com/trab3/Servlet/Logout.java
@@ -1,14 +1,13 @@
 package br.com.trab3.Servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.apache.jasper.tagplugins.jstl.core.Redirect;
 
 /**
  * Servlet implementation class Logout
